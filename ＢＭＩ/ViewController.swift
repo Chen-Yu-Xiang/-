5c0eb6d565283fccec2cbe6d1039851c95924fb6
User21 <user21@user21de-imac.local>
2019-01-13 07:24:35 +0800

    Initial Commit

---
+++ ＢＭＩ/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ＢＭＩ
+//
+//  Created by User21 on 2019/1/13.
+//  Copyright © 2019 User21. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
