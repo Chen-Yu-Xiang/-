0b6d7c4a4bfd1837d3be2466e6a477f49e9af67c
User21 <user21@user21de-imac.local>
2019-01-13 07:37:01 +0800

    bon

--- ＢＭＩ/ViewController.swift
+++ ＢＭＩ/ViewController.swift
@@ -1,20 +1,51 @@
 //
 //  ViewController.swift
-//  ＢＭＩ
+//  Bmi2
 //
-//  Created by User21 on 2019/1/13.
-//  Copyright © 2019 User21. All rights reserved.
+//  Created by SHIH-YING PAN on 2018/11/14.
+//  Copyright © 2018 SHIH-YING PAN. All rights reserved.
 //
 
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var bmiLabel: UILabel!
+    
+    @IBOutlet weak var heightTextField: UITextField!
+    @IBOutlet weak var weightTextField: UITextField!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
-
-
+    
+    @IBAction func a(_ sender: Any) {
+        
+        if let heightText = heightTextField.text, let weightText = weightTextField.text, let height = Double(heightText), let weight = Double(weightText) {
+            let bmi = height/weight
+            bmiLabel.text = bmi.description
+        }
+    }
+    @IBAction func mu(_ sender: Any) {
+        
+        if let heightText = heightTextField.text, let weightText = weightTextField.text, let height = Double(heightText), let weight = Double(weightText) {
+            let bmi = weight*height
+            bmiLabel.text = bmi.description
+        }
+    }
+    @IBAction func down(_ sender: Any) {
+        
+        if let heightText = heightTextField.text, let weightText = weightTextField.text, let height = Double(heightText), let weight = Double(weightText) {
+            let bmi = height-weight
+            bmiLabel.text = bmi.description
+        }
+    }
+    @IBAction func calculate(_ sender: Any) {
+        
+        if let heightText = heightTextField.text, let weightText = weightTextField.text, let height = Double(heightText), let weight = Double(weightText) {
+            let bmi = weight+height
+            bmiLabel.text = bmi.description
+        }
+    }
+    
 }
 
